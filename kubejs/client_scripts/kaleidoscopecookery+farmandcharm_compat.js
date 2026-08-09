936218da24c3b35e39153d360f8801d44843d9d4
Sunny L. <94069216+sunnylin933@users.noreply.github.com>
2026-08-09 11:46:56 -0700

    Commit outstanding KubeJS compat scripts and CBTweaks recipe/loot content
    Adds previously-untracked KubeJS data (rice crop and village chest loot
    tables) and CBTweaks recipe files (zongzi, stockpot rice, tomato
    platter/seed) that existed locally but were never committed. Also
    picks up hand-made tuning since: splitting the farm_charm_delight
    client lang script into per-mod files and adding a donkey burger
    rename, disabling the Kaleidoscope tomato-hiding/recipe-replacement
    blocks in the farm & charm compat script, and swapping the straw hat
    loot table to drop Farmer's Delight cabbage seeds instead of
    Kaleidoscope lettuce seed.

---
+++ kubejs/client_scripts/kaleidoscopecookery+farmandcharm_compat.js
@@ -0,0 +1,3 @@
+ClientEvents.lang('en_us', event => {
+  event.renameItem('kaleidoscope_cookery:donkey_burger', '"Donkey" Burger')
+})
--- kubejs/server_scripts/kaleidoscopecookery+farmandcharm_compat.js
+++ kubejs/server_scripts/kaleidoscopecookery+farmandcharm_compat.js
@@ -1,12 +1,12 @@
-//Tomato Compat
-//Hides Kaleidoscope Cookery tomatoes
-ServerEvents.tags('item', event => {
-  event.add('c:hidden_from_recipe_viewers', 'kaleidoscope_cookery:tomato')
-  event.add('c:hidden_from_recipe_viewers', 'kaleidoscope_cookery:tomato_seed')
-})
-//Moves tag membership from Kaleidoscope Cookery to Let's Do tomatoes
-//KC's pot/stockpot recipes (scramble egg, borscht) use #c:crops/tomato,
-//so after this retag they only accept the Farm & Charm tomato
+// //Tomato Compat
+// //Hides Kaleidoscope Cookery tomatoes
+// ServerEvents.tags('item', event => {
+//   event.add('c:hidden_from_recipe_viewers', 'kaleidoscope_cookery:tomato')
+//   event.add('c:hidden_from_recipe_viewers', 'kaleidoscope_cookery:tomato_seed')
+// })
+// //Moves tag membership from Kaleidoscope Cookery to Let's Do tomatoes
+// //KC's pot/stockpot recipes (scramble egg, borscht) use #c:crops/tomato,
+// //so after this retag they only accept the Farm & Charm tomato
 ServerEvents.tags('item', event => {
     const oldTomato = 'kaleidoscope_cookery:tomato'
     const newTomato = 'farm_and_charm:tomato'
@@ -27,12 +27,12 @@ ServerEvents.tags('item', event => {
     event.add('c:seeds/tomato', 'farm_and_charm:tomato_seeds')
     event.remove('c:seeds/tomato', 'kaleidoscope_cookery:tomato_seed')
 })
-//Replaces Kaleidoscope Cookery tomato recipes
-//(tomato platter hardcodes the KC tomato; seed recipe is redundant since
-//Farm & Charm has its own tomato -> seeds recipe)
-ServerEvents.recipes(event => {
-    event.replaceInput({}, 'kaleidoscope_cookery:tomato', 'farm_and_charm:tomato')
-    event.replaceOutput({}, 'kaleidoscope_cookery:tomato', 'farm_and_charm:tomato')
-    event.remove({ output: 'kaleidoscope_cookery:tomato_seed' })
-    event.replaceInput({}, 'kaleidoscope_cookery:tomato_seed', 'farm_and_charm:tomato_seeds')
-})
+// //Replaces Kaleidoscope Cookery tomato recipes
+// //(tomato platter hardcodes the KC tomato; seed recipe is redundant since
+// //Farm & Charm has its own tomato -> seeds recipe)
+// ServerEvents.recipes(event => {
+//     event.replaceInput({}, 'kaleidoscope_cookery:tomato', 'farm_and_charm:tomato')
+//     event.replaceOutput({}, 'kaleidoscope_cookery:tomato', 'farm_and_charm:tomato')
+//     event.remove({ output: 'kaleidoscope_cookery:tomato_seed' })
+//     event.replaceInput({}, 'kaleidoscope_cookery:tomato_seed', 'farm_and_charm:tomato_seeds')
+// })
